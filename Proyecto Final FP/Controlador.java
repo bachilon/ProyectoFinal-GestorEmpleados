c6711ffe5f56b656864c2ba06acacecb1e3f5325
bachilon <56442519+bachilon@users.noreply.github.com>
2020-05-24 23:50:56 +0200

    subida del proyecto

---
+++ Proyecto Final FP/Controlador.java
@@ -0,0 +1,20 @@
+package Controlador;
+
+import Modelo.conexion;
+import Vista.login;
+
+/**
+ * Esta clase es la ejecutable
+ * @author: Carlos García Acevedo
+ * @version: 23/04/2020
+ */
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		login frame = new login();
+		frame.setVisible(true);
+	}
+
+}
